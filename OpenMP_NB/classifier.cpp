10e7ec324f2555461416ba236fefa6ee137d7031
Shashank Verma <shashank3959@gmail.com>
2020-04-17 19:35:35 -0500

    Run clang-formatter on cpp files

--- OpenMP_NB/classifier.cpp
+++ OpenMP_NB/classifier.cpp
@@ -1,154 +1,149 @@
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <math.h>
-#include <vector>
-#include <map>
-#include <assert.h>
-#include <algorithm>
 #include "classifier.h"
+#include <algorithm>
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <math.h>
 #include <omp.h>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 //#define M_PI 3.14159265359
 
-GaussianNB::GaussianNB() {
-
-}
+GaussianNB::GaussianNB() {}
 
 GaussianNB::~GaussianNB() {}
 
-void GaussianNB::train(vector<vector<double>> data, vector<int> labels)
-{
-	map <int, vector<vector<double>>> lfm;
-	map <int, int> class_count;
-	int train_size = labels.size();
-        int i;
-        cout<<"inside train fn"<<endl;
-	// assign get unique labels to labels_list
-	labels_list_ = labels;
-	std::sort(labels_list_.begin(), labels_list_.end());
-	std::vector<string>::iterator newEnd;
-	// Override duplicate elements
-	//newEnd = std::unique(labels_list_.begin(), labels_list_.end());
-	//labels_list_.erase(newEnd, labels_list_.end());
+void GaussianNB::train(vector<vector<double>> data, vector<int> labels) {
+  map<int, vector<vector<double>>> lfm;
+  map<int, int> class_count;
+  int train_size = labels.size();
+  int i;
+  cout << "inside train fn" << endl;
+  // assign get unique labels to labels_list
+  labels_list_ = labels;
+  std::sort(labels_list_.begin(), labels_list_.end());
+  std::vector<string>::iterator newEnd;
+  // Override duplicate elements
+  // newEnd = std::unique(labels_list_.begin(), labels_list_.end());
+  // labels_list_.erase(newEnd, labels_list_.end());
 
-	features_count_ = data[0].size();
+  features_count_ = data[0].size();
 
-        std::vector<int>::size_type lab=0,l=0;
-        int j=0;
-	// init mean and var
-	#pragma omp parallel private(lab,i,j) num_threads(1)
-	{
-		//#pragma omp section
-		//{
-			#pragma omp for 
-			     for (lab = 0;lab< labels_list_.size();lab++) {
-					class_count[lab] = 0;
-					vector <double> temp(data[0].size(), 0.0);
-					f_stats_[lab].push_back(temp);
-					f_stats_[lab].push_back(temp);
-					f_stats_[lab].push_back(temp);
-				}
-			
-		//}
+  std::vector<int>::size_type lab = 0, l = 0;
+  int j = 0;
+// init mean and var
+#pragma omp parallel private(lab, i, j) num_threads(1)
+  {
+    //#pragma omp section
+    //{
+#pragma omp for
+    for (lab = 0; lab < labels_list_.size(); lab++) {
+      class_count[lab] = 0;
+      vector<double> temp(data[0].size(), 0.0);
+      f_stats_[lab].push_back(temp);
+      f_stats_[lab].push_back(temp);
+      f_stats_[lab].push_back(temp);
+    }
 
-                #pragma omp barrier
-		//gathering data list per class; count classes; sum per class
-		//#pragma omp section
-		//{
-			#pragma omp for 
-			
-				for (i = 0; i < train_size; i++) {
-					lfm[labels[i]].push_back(data[i]); // x_train per class
-					class_count[labels[i]] += 1; // class count
-					for (j = 0; j < features_count_; j++) {
-						f_stats_[labels[i]][0][j] += data[i][j]; // sum per feature
-					}
-				}
-			
-		//}
-		// transforming f_stats 0 sum into mean
-               #pragma omp barrier
+    //}
 
-		//#pragma omp section
-		//{
-			#pragma omp for
-			
-				for (lab = 0;lab< labels_list_.size();lab++) {
-					for (j = 0; j < features_count_; j++) {
-						f_stats_[lab][0][j] /= class_count[lab];
-					}
-					p_class_[lab] = class_count[lab] * 1.0 / labels.size();
+#pragma omp barrier
+    // gathering data list per class; count classes; sum per class
+    //#pragma omp section
+    //{
+#pragma omp for
 
-				}
-			
-		//}
-		// calc var by classes
-                #pragma omp barrier
+    for (i = 0; i < train_size; i++) {
+      lfm[labels[i]].push_back(data[i]); // x_train per class
+      class_count[labels[i]] += 1;       // class count
+      for (j = 0; j < features_count_; j++) {
+        f_stats_[labels[i]][0][j] += data[i][j]; // sum per feature
+      }
+    }
 
-		//#pragma omp section
-		//{
-				#pragma omp for 
-			   
-					for (lab = 0;lab< labels_list_.size();lab++)
-					{
-						for (j = 0; j < features_count_; j++) {
-							for (l = 0; l < lfm[lab].size(); l++) {
-								f_stats_[lab][1][j] += pow(lfm[lab][l][j] - f_stats_[lab][0][j], 2);
-							}
-							f_stats_[lab][1][j] /= class_count[lab];
-							f_stats_[lab][2][j] = 1.0 / sqrt(2 * M_PI * f_stats_[lab][1][j]); // 1/sqrt(2*PI*var)
-						}
-					
-		//}
-	  // }
-	}
+    //}
+    // transforming f_stats 0 sum into mean
+#pragma omp barrier
+
+    //#pragma omp section
+    //{
+#pragma omp for
+
+    for (lab = 0; lab < labels_list_.size(); lab++) {
+      for (j = 0; j < features_count_; j++) {
+        f_stats_[lab][0][j] /= class_count[lab];
+      }
+      p_class_[lab] = class_count[lab] * 1.0 / labels.size();
+    }
+
+//}
+// calc var by classes
+#pragma omp barrier
+
+    //#pragma omp section
+    //{
+#pragma omp for
+
+    for (lab = 0; lab < labels_list_.size(); lab++) {
+      for (j = 0; j < features_count_; j++) {
+        for (l = 0; l < lfm[lab].size(); l++) {
+          f_stats_[lab][1][j] += pow(lfm[lab][l][j] - f_stats_[lab][0][j], 2);
+        }
+        f_stats_[lab][1][j] /= class_count[lab];
+        f_stats_[lab][2][j] =
+            1.0 / sqrt(2 * M_PI * f_stats_[lab][1][j]); // 1/sqrt(2*PI*var)
+      }
+
+      //}
+      // }
+    }
+  }
 }
-}
-int GaussianNB::predict(vector<vector<double>> X_test, vector<int> Y_test)
-{
+int GaussianNB::predict(vector<vector<double>> X_test, vector<int> Y_test) {
 
-	map <int, double> p;
+  map<int, double> p;
 
-	int result=0;
-	int score =0;
-        int i=0;
-        std::vector<int>::size_type lab=0,k;
-	#pragma omp parallel private(lab,k,i) num_threads(1) shared(score)
-	{
-                #pragma omp for 
-		for (k = 0; k < X_test.size(); k++)
-		{
-			vector<double> vec = X_test[k];
+  int result = 0;
+  int score = 0;
+  int i = 0;
+  std::vector<int>::size_type lab = 0, k;
+#pragma omp parallel private(lab, k, i) num_threads(1) shared(score)
+  {
+#pragma omp for
+    for (k = 0; k < X_test.size(); k++) {
+      vector<double> vec = X_test[k];
 
-			//assert(features_count_ == vec.size());
+      // assert(features_count_ == vec.size());
 
+      double max = 0;
+      for (lab = 0; lab < labels_list_.size(); lab++) {
+        p[lab] = p_class_[lab];
+        for (i = 0; i < features_count_; i++) {
+          p[lab] *=
+              f_stats_[lab][2][i] * exp(-pow(vec[i] - f_stats_[lab][0][i], 2) /
+                                        (2 * f_stats_[lab][1][i]));
+        }
 
-			double max = 0;
-			for (lab = 0;lab< labels_list_.size();lab++) {
-				p[lab] = p_class_[lab];
-				for (i = 0; i < features_count_; i++) {
-					p[lab] *= f_stats_[lab][2][i] * exp(-pow(vec[i] - f_stats_[lab][0][i], 2) / (2 * f_stats_[lab][1][i]));
-				}
+        if (max < p[lab]) {
+          max = p[lab];
+          result = lab;
+        }
+      }
+#pragma omp critical
+      if (result == Y_test[k]) {
+        score += 1;
+      }
+    }
+  }
 
-				if (max < p[lab]) {
-					max = p[lab];
-					result = lab;
-				}
-			}
-			#pragma omp critical
-			if (result == Y_test[k])
-			{
-				score += 1;
-			}
-		}
-	}
-
-	return score;
+  return score;
 }
 
-// ******************************** Multinomial Navie Bayes************************************************
+// ******************************** Multinomial Navie
+// Bayes************************************************
 
 /*
 MultionomialGB::MultionomialGB() {
@@ -160,139 +155,149 @@ MultionomialGB::~MultionomialGB() {}
 void MultionomialGB::train(vector<vector<double>> train, vector<int> label)
 {
 
-	//calculate multinomial_sums to find mean and variance
-	
-	double flag;
-	double alpha = 1.0;
-	// Decision rule
-	//int decision = 1;
-	// Verbose
-	//int verbose = 0;
-	cout << "Inside train " << endl;
-	int labels;
-        //#pragma omp parallel 
-	for (auto i = 0; i < train.size(); i++)
-	{
-		labels = label[i];
-		vector<double> values;
-		//cout << "lables" << labels << endl;
-		for (auto l = 1; l < train[0].size(); l++)
-		{
-			flag = train[i][l];
-			//cout << "flag" << flag << endl;
-			values.push_back(flag);
-			//cout << "train 0 size" << train[0].size() << endl;
-			if (sum_x.find(labels) == sum_x.end()) {
-				vector<double> empty;
-				for (unsigned int j = 1; j < train[0].size(); j++) {
-					empty.push_back(0.0);
-				}
-				sum_x[labels] = empty;
-			}
-			
-			sum_x[labels][l - 1] += values[l - 1];
-			multinomial_sums[labels] += values[l - 1];
-			//cout << values.size() << endl;
-			//cout << sum_x[labels].size() << endl;
-			
-			data[labels].push_back(values);
-			n[labels]++;
-			n_total++;
+        //calculate multinomial_sums to find mean and variance
 
-		}
-	}
+        double flag;
+        double alpha = 1.0;
+        // Decision rule
+        //int decision = 1;
+        // Verbose
+        //int verbose = 0;
+        cout << "Inside train " << endl;
+        int labels;
+        //#pragma omp parallel
+        for (auto i = 0; i < train.size(); i++)
+        {
+                labels = label[i];
+                vector<double> values;
+                //cout << "lables" << labels << endl;
+                for (auto l = 1; l < train[0].size(); l++)
+                {
+                        flag = train[i][l];
+                        //cout << "flag" << flag << endl;
+                        values.push_back(flag);
+                        //cout << "train 0 size" << train[0].size() << endl;
+                        if (sum_x.find(labels) == sum_x.end()) {
+                                vector<double> empty;
+                                for (unsigned int j = 1; j < train[0].size();
+j++) { empty.push_back(0.0);
+                                }
+                                sum_x[labels] = empty;
+                        }
 
-	cout << "sum x cal over" << endl;
-	
-	for (auto it = sum_x.begin(); it != sum_x.end(); it++) {
+                        sum_x[labels][l - 1] += values[l - 1];
+                        multinomial_sums[labels] += values[l - 1];
+                        //cout << values.size() << endl;
+                        //cout << sum_x[labels].size() << endl;
 
-		priors[it->first] = (double)n[it->first] / n_total;
+                        data[labels].push_back(values);
+                        n[labels]++;
+                        n_total++;
 
-		if (it->first > 0) {
-			cout<<"class +%i prior: %1.3f\n" <<it->first<<priors[it->first];
-		}
-		else {
-			cout<<"class %i prior: %1.3f\n"<< it->first<<priors[it->first];
-		}
-		cout << "feature\tmean\tvar\tstddev\tmnl" << endl;
+                }
+        }
 
-		// Calculate means
-		vector<double> feature_means;
-		for (unsigned int i = 0; i < it->second.size(); i++) {
-			feature_means.push_back(sum_x[it->first][i] / n[it->first]);
-		}
+        cout << "sum x cal over" << endl;
 
-		// Calculate variances
-		vector<double> feature_variances(feature_means.size());
-		for (unsigned int i = 0; i < data[it->first].size(); i++) {
-			for (unsigned int j = 0; j < data[it->first][i].size(); j++) {
-				feature_variances[j] += (data[it->first][i][j] - feature_means[j]) * (data[it->first][i][j] - feature_means[j]);
-			}
-		}
-		for (unsigned int i = 0; i < feature_variances.size(); i++) {
-			feature_variances[i] /= data[it->first].size();
-		}
+        for (auto it = sum_x.begin(); it != sum_x.end(); it++) {
 
-		// Calculate multinomial likelihoods
-		for (unsigned int i = 0; i < feature_means.size(); i++) {
-			double mnl = (sum_x[it->first][i] + alpha) / (multinomial_sums[it->first] + (alpha * feature_means.size()));
-			//cout << sum_x[it->first][i] << " + 1 / " << multinomial_sums[it->first] << " + " << feature_means.size() << endl;
-			multinomial_likelihoods[it->first].push_back(mnl);
-		}
+                priors[it->first] = (double)n[it->first] / n_total;
 
-		for (unsigned int i = 0; i < feature_means.size(); i++) {
-			printf("%i\t%2.3f\t%2.3f\t%2.3f\t%2.3f\n", i + 1, feature_means[i], feature_variances[i], sqrt(feature_variances[i]), multinomial_likelihoods[it->first][i]);
-			//cout << feature_means[i] << "\t" << sqrt(feature_variances[i]) << endl;
-		}
-		means[it->first] = feature_means;
-		variances[it->first] = feature_variances;
+                if (it->first > 0) {
+                        cout<<"class +%i prior: %1.3f\n"
+<<it->first<<priors[it->first];
+                }
+                else {
+                        cout<<"class %i prior: %1.3f\n"<<
+it->first<<priors[it->first];
+                }
+                cout << "feature\tmean\tvar\tstddev\tmnl" << endl;
 
-	}
+                // Calculate means
+                vector<double> feature_means;
+                for (unsigned int i = 0; i < it->second.size(); i++) {
+                        feature_means.push_back(sum_x[it->first][i] /
+n[it->first]);
+                }
 
-	cout << "training over" << endl;
-	
+                // Calculate variances
+                vector<double> feature_variances(feature_means.size());
+                for (unsigned int i = 0; i < data[it->first].size(); i++) {
+                        for (unsigned int j = 0; j < data[it->first][i].size();
+j++) { feature_variances[j] += (data[it->first][i][j] - feature_means[j]) *
+(data[it->first][i][j] - feature_means[j]);
+                        }
+                }
+                for (unsigned int i = 0; i < feature_variances.size(); i++) {
+                        feature_variances[i] /= data[it->first].size();
+                }
+
+                // Calculate multinomial likelihoods
+                for (unsigned int i = 0; i < feature_means.size(); i++) {
+                        double mnl = (sum_x[it->first][i] + alpha) /
+(multinomial_sums[it->first] + (alpha * feature_means.size()));
+                        //cout << sum_x[it->first][i] << " + 1 / " <<
+multinomial_sums[it->first] << " + " << feature_means.size() << endl;
+                        multinomial_likelihoods[it->first].push_back(mnl);
+                }
+
+                for (unsigned int i = 0; i < feature_means.size(); i++) {
+                        printf("%i\t%2.3f\t%2.3f\t%2.3f\t%2.3f\n", i + 1,
+feature_means[i], feature_variances[i], sqrt(feature_variances[i]),
+multinomial_likelihoods[it->first][i]);
+                        //cout << feature_means[i] << "\t" <<
+sqrt(feature_variances[i]) << endl;
+                }
+                means[it->first] = feature_means;
+                variances[it->first] = feature_variances;
+
+        }
+
+        cout << "training over" << endl;
+
 }
 
 int MultionomialGB::predict(vector<vector<double>> X_test, vector<int> Y_test)
 {
 
-	map <string, double> p;
+        map <string, double> p;
 
-	int result;
-	int score=1;
+        int result;
+        int score=1;
 
-	cout << "inside prediction" << endl;
-	for (auto i = 0; i < X_test.size(); i++)
-	{
-		vector<double> vec = X_test[i];
+        cout << "inside prediction" << endl;
+        for (auto i = 0; i < X_test.size(); i++)
+        {
+                vector<double> vec = X_test[i];
 
-		//assert(features_count_ == vec.size());
-
-
-		double max = 0;
+                //assert(features_count_ == vec.size());
 
 
-		for (auto it = priors.begin(); it != priors.end(); it++) {
-			double numer = priors[it->first];
-			for (auto j = 0; j < features_count_; j++) {
-				numer *= pow(multinomial_likelihoods[it->first][j], vec[j]);
-			}
+                double max = 0;
 
-			if (max < numer) {
-				max = numer;
-				result = it->first;
-			}
-		}
+
+                for (auto it = priors.begin(); it != priors.end(); it++) {
+                        double numer = priors[it->first];
+                        for (auto j = 0; j < features_count_; j++) {
+                                numer *=
+pow(multinomial_likelihoods[it->first][j], vec[j]);
+                        }
+
+                        if (max < numer) {
+                                max = numer;
+                                result = it->first;
+                        }
+                }
 
                 //#pragma omp critical
 
-		if (result==Y_test[i])
-		{
-			score += 1;
-		}
-	}
+                if (result==Y_test[i])
+                {
+                        score += 1;
+                }
+        }
 
-	return score;
+        return score;
 
 
 }
--- OpenMP_NB/main.cpp
+++ OpenMP_NB/main.cpp
@@ -1,149 +1,140 @@
 #include "classifier.h"
-#include <iostream>
-#include <fstream>
-#include <math.h>
 #include <chrono>
-#include <vector>
+#include <fstream>
+#include <iostream>
 #include <map>
+#include <math.h>
 #include <omp.h>
+#include <vector>
 
 using namespace std;
 using std::cout;
-using std::chrono::high_resolution_clock;
 using std::chrono::duration;
+using std::chrono::high_resolution_clock;
 
+vector<vector<double>> Load_State(string file_name) {
+  ifstream in_state_(file_name.c_str(), ifstream::in);
+  vector<vector<double>> state_out;
+  string start;
 
-vector<vector<double> > Load_State(string file_name)
-{
-	ifstream in_state_(file_name.c_str(), ifstream::in);
-	vector< vector<double >> state_out;
-	string start;
+  while (getline(in_state_, start)) {
 
-	while (getline(in_state_, start))
-	{
+    vector<double> x_coord;
 
-		vector<double> x_coord;
+    istringstream ss(start);
+    double a;
+    ss >> a;
+    x_coord.push_back(a);
 
-		istringstream ss(start);
-		double a;
-		ss >> a;
-		x_coord.push_back(a);
+    string value;
 
-		string value;
+    while (getline(ss, value, ',')) {
+      double b;
+      ss >> b;
+      x_coord.push_back(b);
+    }
 
-		while (getline(ss, value, ',')) {
-			double b;
-			ss >> b;
-			x_coord.push_back(b);
-		}
+    state_out.push_back(x_coord);
+  }
 
-		state_out.push_back(x_coord);
-	}
-
-	return state_out;
+  return state_out;
 }
-vector<int> Load_Label(string file_name)
-{
-	ifstream in_label_(file_name.c_str(), ifstream::in);
-	vector< int > label_out;
-	string line;
-	while (getline(in_label_, line))
-	{
-		istringstream iss(line);
-		int label;
-		iss >> label;
+vector<int> Load_Label(string file_name) {
+  ifstream in_label_(file_name.c_str(), ifstream::in);
+  vector<int> label_out;
+  string line;
+  while (getline(in_label_, line)) {
+    istringstream iss(line);
+    int label;
+    iss >> label;
 
-		label_out.push_back(label);
-	}
-	return label_out;
-
+    label_out.push_back(label);
+  }
+  return label_out;
 }
 
 int main() {
 
-	int algoID =0;
-	/*
-	if (string(argv[i]) == "-d") {
-		algoID = atoi(argv[i + 1]);
-	}
-	else {
-		cout << "Invalid option. Code is exiting" << endl;
-		exit(1);
-	}
-	*/
-	high_resolution_clock::time_point start;
-	high_resolution_clock::time_point end;
+  int algoID = 0;
+  /*
+  if (string(argv[i]) == "-d") {
+          algoID = atoi(argv[i + 1]);
+  }
+  else {
+          cout << "Invalid option. Code is exiting" << endl;
+          exit(1);
+  }
+  */
+  high_resolution_clock::time_point start;
+  high_resolution_clock::time_point end;
 
-        vector< vector<double> > X_train;
-        vector< vector<double> > X_test;
-	vector< int > Y_train;
-	vector< int > Y_test; 
-   //lastprivate(X_train,X_test,Y_train,Y_test)
-    #pragma omp parallel sections 
-	{
-		#pragma omp section
-		X_train = Load_State("train_states.csv");
-		
-		#pragma omp section
-		X_test = Load_State("test_states.csv");
-		
-		#pragma omp section
-		Y_train = Load_Label("train_labels.csv");
-		
-		#pragma omp section
-		Y_test = Load_Label("test_labels.csv");
-	}
-	cout << "X_train number of elements " << X_train.size() << endl;
-	cout << "X_train element size " << X_train[0].size() << endl;
-	cout << "Y_train number of elements " << Y_train.size() << endl << endl;
+  vector<vector<double>> X_train;
+  vector<vector<double>> X_test;
+  vector<int> Y_train;
+  vector<int> Y_test;
 
+  // lastprivate(X_train,X_test,Y_train,Y_test)
+#pragma omp parallel sections
+  {
+#pragma omp section
+    X_train = Load_State("train_states.csv");
 
-	cout << "X_test number of elements " << X_test.size() << endl;
-	cout << "X_test element size " << X_test[0].size() << endl;
-	cout << "Y_test number of elements " << Y_test.size() << endl << endl;
+#pragma omp section
+    X_test = Load_State("test_states.csv");
 
-	if (algoID == 0)
-	{
-		duration<double, std::milli> duration_sec;
-		start = high_resolution_clock::now();
-                cout<<"calling guassioan"<<endl;
-		GaussianNB model = GaussianNB();
-		model.train(X_train, Y_train);
+#pragma omp section
+    Y_train = Load_Label("train_labels.csv");
 
-		end = high_resolution_clock::now();
-		duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+#pragma omp section
+    Y_test = Load_Label("test_labels.csv");
+  }
+  cout << "X_train number of elements " << X_train.size() << endl;
+  cout << "X_train element size " << X_train[0].size() << endl;
+  cout << "Y_train number of elements " << Y_train.size() << endl << endl;
 
-		cout << "training time "<<duration_sec.count() << endl;
+  cout << "X_test number of elements " << X_test.size() << endl;
+  cout << "X_test element size " << X_test[0].size() << endl;
+  cout << "Y_test number of elements " << Y_test.size() << endl << endl;
 
-		int score = 0;
-		score = model.predict(X_test, Y_test);
+  if (algoID == 0) {
+    duration<double, std::milli> duration_sec;
+    start = high_resolution_clock::now();
+    cout << "calling guassioan" << endl;
+    GaussianNB model = GaussianNB();
+    model.train(X_train, Y_train);
 
-		float fraction_correct = float(score) / Y_test.size();
-		cout << "You got " << (100 * fraction_correct) << " correct" << endl;
-	}
-	
-/*
-	else if (algoID == 1)
-	{
-		
-		MultionomialGB model = MultionomialGB();
-		cout << "Calling train" << endl;
-		model.train(X_train, Y_train);
+    end = high_resolution_clock::now();
+    duration_sec =
+        std::chrono::duration_cast<duration<double, std::milli>>(end - start);
 
+    cout << "training time " << duration_sec.count() << endl;
 
-		int score = 0;
+    int score = 0;
+    score = model.predict(X_test, Y_test);
 
-		score = model.predict(X_test, Y_test);
+    float fraction_correct = float(score) / Y_test.size();
+    cout << "You got " << (100 * fraction_correct) << " correct" << endl;
+  }
 
-		float fraction_correct = float(score) / Y_test.size();
-		cout << "You got " << (100 * fraction_correct) << " correct" << endl;
-	}
-	
-*/
+  /*
+          else if (algoID == 1)
+          {
+
+                  MultionomialGB model = MultionomialGB();
+                  cout << "Calling train" << endl;
+                  model.train(X_train, Y_train);
 
 
+                  int score = 0;
 
-	return 0;
+                  score = model.predict(X_test, Y_test);
+
+                  float fraction_correct = float(score) / Y_test.size();
+                  cout << "You got " << (100 * fraction_correct) << " correct"
+     << endl;
+          }
+
+  */
+
+  return 0;
 }
-
-
